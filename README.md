7130533ccf9b4832e3f48e189f2f96ae4726e4f8
stigma0617 <sun008sun@naver.com>
2019-06-14 16:39:11 +0900

    initial VoVNet

--- README.md
+++ README.md
@@ -1,2 +1,60 @@
-# VoVNet.pytorch
-A pytorch implementation of VoVNet
+# A pytorch Implementation of VoVNet Backbone Networks
+
+This is a pytorch implementation of VoVNet backbone networks as described in the paper [An Energy and GPU-Computation Efficient Backbone Network for Real-Time Object Detection](https://arxiv.org/abs/1904.09730). This is implemented through [pytorch/vision](https://github.com/pytorch/vision/tree/master/torchvision/models) style.
+
+## What does this repo provide?  
+This repo provides VoVNet-39/57 models trained on ImageNet classification dataset with same training protocols as [pytorch/examples/imagenet](https://github.com/pytorch/examples/tree/master/imagenet) (e.g., 128 batch size, 90 epoch, data augmentations, lr_policy, etc) and compares to ResNet and DenseNet.
+
+
+## ImageNet results
+
+### Notes:
+ - For fair comparison, same training protocols used as [pytorch/examples/imagenet](https://github.com/pytorch/examples/tree/master/imagenet)
+    - 90 epoch
+    - step learning rate schedule (learning rate decayed at every 30 epoch)
+    - 256 batch size
+    - default augmentations (e.g., crop, flip, same mean/std normalization)
+    - @224x224 training/validation
+ - Inference time is measured on TITAN X PASCAL GPU
+    - CUDA v9.2, cuDNN v7.3, pytorch 1.0
+ 
+
+| Model | Top-1 | Top-5 | Inference time
+| :--:  |  :--: | :--:  | :--: |
+| ResNet-50     |  23.85%     | 7.13%     |12 ms|
+| DenseNet-201  |  22.80%     | 6.43%     |39 ms|
+| **VoVNet-39** |  23.23%     | 6.57%     |**10** ms|
+| ResNet-101    |  22.63%     | 6.44%     |20 ms|
+| DenseNet-161  |  22.35%     | 6.20%     |27 ms|
+| **VoVNet-57** |  **22.27**% | **6.28**% |13 ms |
+
+
+
+## Pretrained models
+
+- [VoVNet-39](https://www.dropbox.com/s/1lnzsgnixd8gjra/vovnet39_torchvision.pth)
+- [VoVNet-57](https://www.dropbox.com/s/6bfu9gstbwfw31m/vovnet57_torchvision.pth)
+
+
+## Training & Inferecne
+
+### Installation
+0. python  3.6
+1. Install pytorch > 0.4
+2. `git clone https://github.com/stigma0617/VoVNet.pytorch.git`
+3. install depenecies : `pip install -r requirements.txt`
+
+### Imagenet data preparation
+
+You can follow the instruction of [pytorch/examples/imagenet](https://github.com/pytorch/examples/tree/master/imagenet)
+
+
+### Train
+
+run `main.py` specifying data path, the desired model name`--arch`, and save-directory`--savedir`.
+
+
+````bash
+python main.py [imagenet-folder with train and val folders] --arch vovnet39 --savedir VoVNet39
+````
+
---
+++ main.py
@@ -0,0 +1,338 @@
+import argparse
+import os
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+
+import models_vovnet
+
+model_names = sorted(name for name in models.__dict__
+    if name.islower() and not name.startswith("__")
+    and callable(models.__dict__[name]))
+
+model_names = model_names + sorted(name for name in models_vovnet.__dict__ 
+    if name.islower() and not name.startswith("__") 
+    and callable(models_vovnet.__dict__[name]))
+
+
+parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+parser.add_argument('data', metavar='DIR',
+                    help='path to dataset')
+parser.add_argument('--arch', '-a', metavar='ARCH', default='vovnet69v2',
+                    #choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('--size', default=224, type=int, metavar='N',
+                    help='input dimension')
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+parser.add_argument('--epochs', default=90, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=256, type=int,
+                    metavar='N', help='mini-batch size (default: 256)')
+parser.add_argument('-tb', '--test-batch-size', default=128, type=int,
+                    metavar='N', help='mini-batch size (default: 128)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    metavar='N', help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+parser.add_argument('--pretrained', dest='pretrained', action='store_true',
+                    help='use pre-trained model')
+parser.add_argument('--savedir', required=True)
+
+
+best_prec1 = 0
+
+def main():
+    global args, best_prec1
+    args = parser.parse_args()
+
+    # create model
+    if args.pretrained:
+        print("=> using pre-trained model '{}'".format(args.arch))
+        # model = models.__dict__[args.arch](pretrained=True)
+        if 'vovnet' in args.arch:
+            model = models_vovnet.__dict__[args.arch](pretrained=True)
+        else:
+            model = models.__dict__[args.arch](pretrained=True)
+    else:
+        print("=> creating model '{}'".format(args.arch))
+        if 'vovnet' in args.arch:
+            model = models_vovnet.__dict__[args.arch]()
+        else:
+            model = models.__dict__[args.arch]()
+
+    model = torch.nn.DataParallel(model).cuda()
+
+    # define loss function (criterion) and optimizer
+    criterion = nn.CrossEntropyLoss().cuda()
+
+    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+                                momentum=args.momentum,
+                                weight_decay=args.weight_decay)
+
+    savedir = f'./checkpoint/{args.savedir}'
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+
+    log_path = savedir+"/training_log.txt"
+    if (not os.path.exists(log_path)):
+        with open(log_path, "a") as myfile:
+            myfile.write("Epoch\t\tTrain-loss\t\tTest-loss\t\tpred@1\t\tpred@5\t\tlearningRate")
+
+    # optionally resume from a checkpoint
+    if args.resume:
+        if os.path.isfile(args.resume):
+            print("=> loading checkpoint '{}'".format(args.resume))
+            checkpoint = torch.load(args.resume)
+            args.start_epoch = checkpoint['epoch']
+            best_prec1 = checkpoint['best_prec1']
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(args.resume, checkpoint['epoch']))
+        else:
+            print("=> no checkpoint found at '{}'".format(args.resume))
+
+    cudnn.benchmark = True
+
+    # Data loading code
+    traindir = os.path.join(args.data, 'train')
+    valdir = os.path.join(args.data, 'val')
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+
+    model_input_size = args.size
+    resize = 256
+    if args.size == 299:
+        resize = 299
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(traindir, transforms.Compose([
+            transforms.RandomResizedCrop(model_input_size),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.batch_size, shuffle=True,
+        num_workers=args.workers, pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(valdir, transforms.Compose([
+            transforms.Resize(resize),
+            transforms.CenterCrop(model_input_size),
+            transforms.ToTensor(),
+            normalize,
+        ])),
+        batch_size=args.test_batch_size, shuffle=False,
+        num_workers=args.workers, pin_memory=True)
+
+    if args.evaluate:
+        validate(val_loader, model, criterion)
+        return
+
+    for epoch in range(args.start_epoch, args.epochs):
+        adjust_learning_rate(optimizer, epoch, args)
+
+        # train for one epoch
+        train_losses = train(train_loader, model, criterion, optimizer, epoch)
+
+        # evaluate on validation set
+        prec1, prec5, val_losses = validate(val_loader, model, criterion)
+
+        # remember best prec@1 and save checkpoint
+        is_best = prec1 > best_prec1
+        best_prec1 = max(prec1, best_prec1)
+        filenameCheckpoint = savedir + '/checkpoint.pth.tar'
+        filenameBest = savedir + '/model_best.pth.tar'.format(epoch+1, prec1, prec5)
+        save_checkpoint({
+            'epoch': epoch + 1,
+            'arch': args.arch,
+            'state_dict': model.state_dict(),
+            'best_prec1': best_prec1,
+            'optimizer' : optimizer.state_dict(),
+        }, is_best, filenameCheckpoint, filenameBest)
+
+        if is_best:
+            with open(savedir+"/best.txt", "w") as myfile:
+                myfile.write("Best accuracy : (%.4f, %.4f) at %d epoch" 
+                    % (prec1, prec5, epoch))
+
+        #save log
+        LR = 0
+        for param_group in optimizer.param_groups:
+            LR = float(param_group['lr'])
+
+        with open(log_path, "a") as myfile:
+            myfile.write("\n%d\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.3f\t\t%.8f"
+                % (epoch, train_losses.avg, val_losses.avg, prec1, prec5, LR))
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to train mode
+    model.train()
+
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input)
+        target_var = torch.autograd.Variable(target)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        lr = 0
+        if i % args.print_freq == 0:
+            for param_group in optimizer.param_groups:
+                lr = param_group['lr']
+            print('Epoch: [{0}][{1}/{2}][lr:{lr:.6g}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.2f}) / '
+                  'Data {data_time.val:.3f} ({data_time.avg:.2f})\t'
+                  'Loss {loss.val:.3f} ({loss.avg:.3f})\t'
+                  'Prec@1 {top1.val:.2f} ({top1.avg:.2f})\t'
+                  'Prec@5 {top5.val:.2f} ({top5.avg:.2f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top5=top5, lr=lr))
+
+    return losses
+
+
+def validate(val_loader, model, criterion):
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top5 = AverageMeter()
+
+    # switch to evaluate mode
+    model.eval()
+
+    end = time.time()
+    for i, (input, target) in enumerate(val_loader):
+        target = target.cuda(async=True)
+        input_var = torch.autograd.Variable(input, volatile=True)
+        target_var = torch.autograd.Variable(target, volatile=True)
+
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+
+        # measure accuracy and record loss
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        losses.update(loss.data[0], input.size(0))
+        top1.update(prec1[0], input.size(0))
+        top5.update(prec5[0], input.size(0))
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        if i % args.print_freq == 0:
+            print('Test: [{0}/{1}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                   i, len(val_loader), batch_time=batch_time, loss=losses,
+                   top1=top1, top5=top5))
+
+    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+          .format(top1=top1, top5=top5))
+
+    return top1.avg, top5.avg, losses
+
+
+def save_checkpoint(state, is_best, filenameCheckpoint, filenameBest):
+    torch.save(state, filenameCheckpoint)
+    if is_best:
+        print("Saving model as best")
+        torch.save(state, filenameBest)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """Sets the learning rate to the initial LR decayed
+    by 10 every 30 epochs"""
+    lr = args.lr * (0.1 ** (epoch // 30))
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
+if __name__ == '__main__':
+    main()
